508c6addb5a0fd583e25dce0679a88386e56117a
benjamin thom, dusty mccord and jonathan carlos <dusty@>
2020-01-13 11:46:34 -0800

    add document ready command in jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,48 @@
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
 
-alert(add(number1, number2));
-alert(subtract(number1, number2));
-alert(multiply(number1, number2));
-alert(divide(number1, number2));
+// alert(add(number1, number2));
+// alert(subtract(number1, number2));
+// alert(multiply(number1, number2));
+// alert(divide(number1, number2));
 
-// var result = add(number1, number2);
+// // var result = add(number1, number2);
 
 
-    var add = function(number1, number2) {
-        return number1 + number2;
-    };
+// var add = function(number1, number2) {
+//     return number1 + number2;
+// };
+
+// var subtract = function(number1, number2) {
+//     return number1 - number2;
+// };
+
+// var multiply = function(number1, number2) {
+//     return number1 * number2;
+// };
+
+// var divide = function(number1, number2) {
+//     return number1 / number2;
+// };
+
+$(document).ready(function(){
+
+  $("h1").click(function() {
+    alert("This is a header.");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+
+});
+
+
+// var header = document.getElementById('head').addEventListener('click', function(){
+//   alert('test');
+// });
